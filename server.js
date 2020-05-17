f40304af6ac266c794250f276aabeb90ca902993
unknown <catalin_vl67@yahoo.com>
2020-05-17 17:56:42 +0300

    '17mai'

---
+++ server.js
@@ -0,0 +1,29 @@
+
+const express = require("express");
+const Router = require('express').Router();
+const HttpStatus = require('http-status-codes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+const data = [];
+
+app.use(bodyParser.json())
+
+Router.post("/salut" , (req,res) =>{
+    data.push(req.body.email);
+    res.status(HttpStatus.OK).json(req.body.email);
+    //console.log(data);
+})
+Router.get("/salut" , (req,res) =>{
+    console.log(data);
+    res.status(HttpStatus.OK).json(data);
+})
+
+app.use(cors());
+app.use(Router);
+
+app.listen(3005 , ()=>{
+    console.log("App is listening on port 3005");
+});
+
