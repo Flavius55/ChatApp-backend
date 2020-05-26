e3a999ddd48a52570f26c83b82afb329d86b904c
unknown <catalin_vl67@yahoo.com>
2020-05-26 20:10:59 +0300

    miercuri

--- server.js
+++ server.js
@@ -5,26 +5,35 @@ const HttpStatus = require('http-status-codes');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const Datastore = require('nedb');
 require('dotenv').config();
 
-const data = [{key:"value"}];
+const database = new Datastore("./stored_things/database.db");
+database.loadDatabase();
+//database.insert({name:"flavius"});
 
 app.use(bodyParser.json())
-
-
 Router.post("/" , (req,res) =>{
-    data.push(req.body.email);
-    res.status(HttpStatus.OK).json(req.body.email);
-    //console.log(data);
+    database.insert({
+        name:req.body.name,
+        mesage:req.body.mesage
+    });
+    res.status(HttpStatus.OK).json({
+        name:req.body.name,
+        mesage:req.body.mesage
+    });
+})
+Router.get("/" , (req,res) =>{
+    database.find({} , (err,data)=>{
+        console.log(data);
+        res.status(HttpStatus.OK).json(data);
+    });
+
+    
 })
 
-Router.get("/" , (req,res) =>{
-    console.log(data);
-    res.status(HttpStatus.OK).json(data);
-})
 
 app.use(cors());
-//app.use(express.static("public"));
 app.use(Router);
 
 app.listen(process.env.PORT , ()=>{
